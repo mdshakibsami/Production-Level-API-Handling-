274b7968cc80dfa5d2880204e88495c1a9a69f02
mdshakibsami <mdshakibsami21@gmail.com>
2025-08-03 00:58:59 +0600

    created customeReactQuery hook

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { useEffect } from "react";
 import axios from "axios";
-function App() {
+
+// hook name must be start with "use"
+const useCustomReactQuery = (url) => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [search,setSearch]=useState('')
 
   useEffect(() => {
     // Effi
-    ;(async () => {
+    (async () => {
       try {
         setError(false);
         setLoading(true);
-        const response = await axios.get(`/api/products?search=${search}`);
+        const response = await axios.get(url);
         setProducts(response.data);
         console.log(response.data);
         setLoading(false);
       } catch (error) {
         setError(true);
-        setLoading(false)
-        console.log(error)
+        setLoading(false);
+        console.log(error);
       }
     })();
-  }, [search]);
+  }, [url]);
+
+  return [products, error, loading];
+};
+
+function App() {
+  const [search, setSearch] = useState("");
+
+  const [products, error, loading] = useCustomReactQuery(
+    `/api/products?search=${search}`
+  );
 
   if (error) return <p>Something went wrong!</p>;
 
@@ -35,7 +45,11 @@ function App() {
     <>
       <h1>Handle API like a professional</h1>
       <h2 className="text-3xl my-5">Number of Products : {products.length}</h2>
-      <input onChange={(e)=>setSearch(e.target.value)} className="my-2 bg-white text-black border-3 rounded-lg w-1/2 py-1 px-2 border-red-500" placeholder="search food item name"></input>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        className="my-2 bg-white text-black border-3 rounded-lg w-1/2 py-1 px-2 border-red-500"
+        placeholder="search food item name"
+      ></input>
       <div className="grid grid-cols-4 gap-5">
         {products.map((product) => {
           return (
